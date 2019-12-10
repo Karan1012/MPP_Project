c14c8dc757518c168b5f5e08fb35b975e71ee962
Cat Stone <stonecat@vt.edu>
2019-12-09 20:44:45 -0500

    stuff

--- dynaq/world_agent.py
+++ dynaq/world_agent.py
@@ -222,6 +222,6 @@ class DynaQWorldAgent(mp.Process):
             finally:
                 self.world_lock.release()
 
-            if self.losses.avg < 50 and (t_step % self.num_threads == 0):
+            if self.losses.avg < 50 and (t_step % 100 == 0):
                 self.planning()
 
