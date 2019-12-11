43e93c05aca219c8377f99be886ac96ccd3d6352
Cat Stone <stonecat@vt.edu>
2019-12-11 15:55:03 -0500

    add layers to a3c

--- a3c/model.py
+++ a3c/model.py
@@ -5,18 +5,22 @@ class ActorCriticNetwork(nn.Module):
 
     def __init__(self, input_dim, output_dim):
         super(ActorCriticNetwork, self).__init__()
-        self.policy1 = nn.Linear(input_dim, 256)
-        self.policy2 = nn.Linear(256, output_dim)
+        self.policy1 = nn.Linear(input_dim, 64)
+        self.policy2 = nn.Linear(64, 64)
+        self.policy3 = nn.Linear(64, output_dim)
 
-        self.value1 = nn.Linear(input_dim, 256)
-        self.value2 = nn.Linear(256, 1)
+        self.value1 = nn.Linear(input_dim, 64)
+        self.value2 = nn.Linear(64, 64)
+        self.value3 = nn.Linear(64, 1)
 
     def forward(self, state):
         logits = F.relu(self.policy1(state))
-        logits = self.policy2(logits)
+        logits = F.relu(self.policy2(logits))
+        logits = self.policy3(logits)
 
         value = F.relu(self.value1(state))
-        value = self.value2(value)
+        value = F.relu(self.value2(value))
+        value = self.value3(value)
 
         return logits, value
 
